403df0e1aded8c301b7feffff32f1de04481f0b8
jonsilvestrini <jonatas.silvestrini@devcoffee.com.br>
2019-02-20 09:01:25 -0300

    método de conversão b64 para imagem adicionada na classe BinaryData

--- src/BinaryData.class.php
+++ src/BinaryData.class.php
@@ -5,5 +5,10 @@ class BinaryData {
             $base64 = base64_encode($data);
             return $base64;
         }
+
+        public function base64img($b64code){
+            $img = base64_decode($b64code);
+            return $img;
+        }
 }
 ?>
