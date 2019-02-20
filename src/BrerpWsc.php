6f59cef67280cdeb1bb7d3b2fb9c4681faeae4d9
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2019-02-20 14:56:10 -0300

    Adicionando Cabeçalhos

--- src/BrerpWsc.php
+++ src/BrerpWsc.php
@@ -1,3 +1,25 @@
+"""
+Produto: BrERP Web Service Client - PHP                                                   
+Copyright (C) 2018  devCoffee Sistemas de Gestão Integrada                 
+                                                                           
+Este arquivo é parte do BrERP Web Service Client - PHP que é software livre; você pode     
+redistribuí-lo e/ou modificá-lo sob os termos da Licença Pública Geral GNU,
+conforme publicada pela Free Software Foundation; tanto a versão 3 da      
+Licença como (a seu critério) qualquer versão mais nova.                   
+                                                                           
+                                                                           
+Este programa é distribuído na expectativa de ser útil, mas SEM            
+QUALQUER GARANTIA; sem mesmo a garantia implícita de                       
+COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM                    
+PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais           
+detalhes.                                                                  
+                                                                           
+Você deve ter recebido uma cópia da Licença Pública Geral GNU              
+junto com este programa; se não, escreva para a Free Software              
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                   
+02111-1307, USA  ou para devCoffee Sistemas de Gestão Integrada,           
+Rua Paulo Rebessi 665 - Cidade Jardim - Leme/SP - Brasil.                           
+"""
 <?php
 
 //namespace BrerpPhpCompositeWsc;
